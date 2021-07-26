aebeed1edb80e1ef2fed647f231322bf76b180b7
Mousa Adolai <mousa.adolai@gmail.com>
2021-07-26 12:51:42 -0400

    Test cases completed

--- src/day1/ResettingPractice.java
+++ src/day1/ResettingPractice.java
@@ -4,6 +4,9 @@ public class ResettingPractice {
 
     public static void main(String[] args) {
         System.out.println("I love git");
+        //p4
+        //p5
+        //p4 complete p5 complete in one commit
     }
 
 }
