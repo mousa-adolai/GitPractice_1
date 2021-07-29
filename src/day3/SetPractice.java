5ea7915105e944376ade63c3ee9f04a0f08d61a5
Mousa Adolai <mousa.adolai@gmail.com>
2021-07-29 14:42:27 -0400

    done1

--- src/day3/SetPractice.java
+++ src/day3/SetPractice.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class SetPractice {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
-
+        for (int i = 0; i < 24; i++) {
+            list.add("ye");
+        }
     }
 }
