00be84b1891f6f8288a7f4fb61890c3923a4014b
Mousa Adolai <mousa.adolai@gmail.com>
2021-07-29 14:41:24 -0400

    done

---
+++ src/day3/SetPractice.java
@@ -0,0 +1,10 @@
+package day3;
+
+import java.util.ArrayList;
+
+public class SetPractice {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+    }
+}
