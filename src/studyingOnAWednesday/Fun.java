706a73c37804d6ae1257a6be4d6f24de0a5e4930
Mousa Adolai <mousa.adolai@gmail.com>
2021-08-04 10:16:27 -0400

    made a fun project on seperate branch

---
+++ src/studyingOnAWednesday/Fun.java
@@ -0,0 +1,11 @@
+package studyingOnAWednesday;
+
+public class Fun {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = i; i >= 0; j--) {
+                System.out.println("*");
+            }
+        }
+    }
+}
